23ac10ebded1e6c8a7f436124b41614bd12f975f
bellergy <rc@bellergy.com>
2018-07-20 23:11:52 +0800

    Update file path; Remove IE 8, 9 fallback js

--- src/js/config.js
+++ src/js/config.js
@@ -3,10 +3,3 @@
 // console.log = function() {};
 
 console.log('Hello, config.js');
-
-// define backbone namespace
-window.Config = {
-    api: "http://iems.ust.dq.hk/api/",
-    staticFolder:"",
-    months:["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"]
-};
